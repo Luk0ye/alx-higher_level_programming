4fa1e91e8812ff12ab64ca9c281d47c42a00ba35
Luk0ye <slukoye01@gmail.com>
2023-12-19 20:41:20 +0300

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A group of functions that define a square class"""
+
+
+class Square:
+    """This Class represents a square"""
+    def __init__(self, size=0):
+        """Initialize the Square
+
+        Args:
+            size (int): size of the Square (length of one side)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    # define the area method
+    def area(self):
+        """returns the area of the square"""
+        return self.__size**2
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A group of functions that define a square class"""
+
+
+class Square:
+    """This Class represents a square"""
+    def __init__(self, size=0):
+        """Initialize the Square
+
+        Args:
+            size (int): size of the Square (length of one side)
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    # define the area method
+    def area(self):
+        """returns the area of the square"""
+        return self.size**2
